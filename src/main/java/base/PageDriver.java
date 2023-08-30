510440fc0b9bf7237c82e917cc2efd8efd18ae55
nikitavpatil74 <135006882+nikitavpatil74@users.noreply.github.com>
2023-08-30 08:21:13 +0000

    Update PageDriver.java

--- src/main/java/base/PageDriver.java
+++ src/main/java/base/PageDriver.java
@@ -1,7 +1,8 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PageDriver {
     private static final ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
